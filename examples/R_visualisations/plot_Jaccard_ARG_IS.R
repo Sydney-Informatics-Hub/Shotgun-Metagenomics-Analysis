f0980ccda0c98507808ea9eb4f1e67ef5d3a76f7
FangWang <77766892+fancyge@users.noreply.github.com>
2023-02-02 21:30:39 +1100

    Add files via upload

--- examples/R_visualisations/plot_Jaccard_ARG_IS.R
+++ examples/R_visualisations/plot_Jaccard_ARG_IS.R
@@ -14,15 +14,22 @@ jaccard <- function(a, b) {
 library(ggplot2)
 library(reshape2)
 
-dtp5<-read.delim("jcc_arg_is_sp.txt")
-d_tp5<-melt(dtp5, id.vars="timepoint")
-names(d_tp5) <- c("Timepoint", "variable","Jaccard_index")
+dtp<-read.delim("jcc_arg_is_sp.txt")
+d_tp<-melt(dtp, id.vars="timepoint")
+names(d_tp) <- c("Timepoint", "variable","Jaccard_index")
 
-d_tp5<-d_tp5[c(1:12,25:36,61:72),]
-d_tp5$type<- c(rep("Resistome",12),rep("Insertion_sequences",12),rep("Microbiome",12))
+d_tp<-d_tp[c(1:12,13:24,25:36),]
+d_tp$type<- c(rep("Resistome",12),rep("Insertion_sequences",12),rep("Microbiome",12))
 
-d_tp5$groups<-c(rep("healthy",3),rep("mild",3),rep("moderate",3),rep("severe",3),rep("healthy",3),rep("mild",3),rep("moderate",3),rep("severe",3),rep("healthy",3),rep("mild",3),rep("moderate",3),rep("severe",3))
+d_tp$groups<-c(rep("healthy",3),rep("mild",3),rep("moderate",3),rep("severe",3),rep("healthy",3),rep("mild",3),rep("moderate",3),rep("severe",3),rep("healthy",3),rep("mild",3),rep("moderate",3),rep("severe",3))
 
 
-ggplot(d_tp5, aes(Timepoint, Jaccard_index, group = variable, shape = type,color = groups,)) + geom_line(linetype = c(rep("dotted",12),rep("solid",12),rep("dotdash",12))) + theme_bw()+  geom_point(size=2.5)+scale_color_manual(values=c('dark green','dark orange','purple','red','black','purple','red','#E69F00','green','blue','black','purple','red','#E69F00','purple','blue','black','purple','red','#E69F00','green','blue','black','purple'))+theme(legend.text = element_text(size=8),legend.key.size = unit(0.5, 'cm'))
+ggplot(d_tp, aes(Timepoint, Jaccard_index, group = variable, shape = type,color = groups,)) + geom_line(linetype = c(rep("dotted",12),rep("solid",12),rep("dotdash",12))) + theme_bw()+  geom_point(size=2.5)+scale_color_manual(values=c('dark green','dark orange','purple','red','black','purple','red','#E69F00','green','blue','black','purple','red','#E69F00','purple','blue','black','purple','red','#E69F00','green','blue','black','purple'))+theme(legend.text = element_text(size=8),legend.key.size = unit(0.5, 'cm'))
 
+
+
+
+"#E64626", "#0148A4", "#FFB800", "#007E3B"
+
+
+ggplot(d_tp, aes(Timepoint, Jaccard_index, group = variable, shape = type,color = groups,)) + geom_line(linetype = c(rep("dotted",12),rep("solid",12),rep("dotdash",12))) + theme_bw()+  geom_point(size=2.5)+scale_color_manual(values=c("#E64626", "#0148A4", "#FFB800", "#007E3B"))+theme(legend.text = element_text(size=8),legend.key.size = unit(0.5, 'cm'))
